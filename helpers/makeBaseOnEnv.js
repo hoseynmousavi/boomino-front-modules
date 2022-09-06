ddde81f8c7aeb3eb57d7ff4ec15cca473b7ce545
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-09-06 12:30:36 +0430

    better base on env

--- helpers/makeBaseOnEnv.js
+++ helpers/makeBaseOnEnv.js
@@ -1,9 +1,18 @@
 function makeBaseOnEnv(base)
 {
     const {hostname} = window.location
-    if (process.env.NODE_ENV === "development" || hostname.includes("dev")) return base.replace(".boomino.ir", "-dev.boomino.ir")
-    else if (hostname.includes("uat")) return base.replace(".boomino.ir", "-uat.boomino.ir")
-    else return base
+    if (process.env.NODE_ENV === "development" || hostname.includes("dev"))
+    {
+        return base.replace(`.${process.env.REACT_APP_DOMAIN}`, `-dev.${process.env.REACT_APP_DOMAIN}`)
+    }
+    else if (hostname.includes("uat"))
+    {
+        return base.replace(`.${process.env.REACT_APP_DOMAIN}`, `-uat.${process.env.REACT_APP_DOMAIN}`)
+    }
+    else
+    {
+        return base
+    }
 }
 
 export default makeBaseOnEnv
