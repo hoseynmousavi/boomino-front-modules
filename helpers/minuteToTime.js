689468a0d44fa2bf569edc71705800f92815ab25
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-09-23 20:45:48 +0330

    seconds added to time

--- helpers/minuteToTime.js
+++ helpers/minuteToTime.js
@@ -1,12 +1,13 @@
 import fixTimeFormat from "./fixTimeFormat"
 
-function minuteToTime({timeMinutes, withLetter})
+function minuteToTime({timeMinutes, timeSeconds, withLetter})
 {
     const hour = Math.floor(timeMinutes / 60)
     const minute = Math.floor(timeMinutes % 60)
+    const second = timeSeconds
     if (withLetter)
     {
-        let time = `${hour ? hour + " ساعت" : ""}${hour && minute ? " و " : ""}${minute ? minute + " دقیقه" : ""}`
+        let time = `${hour ? hour + " ساعت" : ""}${hour && minute ? " و " : ""}${minute ? minute + " دقیقه" : ""}${(hour || minute) && second ? " و " : ""}${second ? second + " ثانیه" : ""}`
         if (time === "15 دقیقه") time = "یک ربع ساعت"
         else if (time === "30 دقیقه") time = "نیم ساعت"
         if (time) return time
