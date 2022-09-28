cacf489dc47e86408b13443bc4a4e044a5266328
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-09-28 15:21:36 +0330

    btn bottom refactor

--- components/BtnBottomFullScreen.js
+++ components/BtnBottomFullScreen.js
@@ -2,14 +2,14 @@ import {useLayoutEffect, useRef} from "react"
 import GetTheme from "../../seyed-modules/hooks/GetTheme"
 import checkIsPinned from "../helpers/checkIsPinned"
 
-function BtnBottomFullScreen({className, children, changeOnDark = true})
+function BtnBottomFullScreen({className, children, delayCheckPin, changeOnDark = true})
 {
     const btnRef = useRef(null)
     const {isDark} = GetTheme()
 
     useLayoutEffect(() =>
     {
-        setTimeout(() => checkIsPinned({ref: btnRef}), 300)
+        setTimeout(() => checkIsPinned({ref: btnRef}), delayCheckPin ? 300 : 0)
         // eslint-disable-next-line
     }, [])
 
