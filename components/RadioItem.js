ffd4a39101f328bb4ce6a3d3b86648105b209548
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-10-03 14:24:24 +0330

    add class to radio

--- components/RadioItem.js
+++ components/RadioItem.js
@@ -2,10 +2,10 @@ import RadioBtnFillSvg from "../media/svg/RadioBtnFillSvg"
 import RadioBtnSvg from "../media/svg/RadioBtnSvg"
 import Material from "../../seyed-modules/components/Material"
 
-function RadioItem({onClick, name, isActive, isRtl, rtlExtraContent})
+function RadioItem({onClick, className, name, isActive, isRtl, rtlExtraContent})
 {
     return (
-        <Material className="select-item" onClick={onClick}>
+        <Material className={`select-item ${className}`} onClick={onClick}>
             <div className={`select-item-title ${isRtl ? "rtl" : ""}`}>{name}</div>
             <RadioBtnFillSvg className={`select-item-radio ${isRtl ? "rtl" : ""} ${isActive ? "show" : ""}`}/>
             <RadioBtnSvg className={`select-item-radio ${isRtl ? "rtl" : ""} ${isActive ? "" : "show"}`}/>
