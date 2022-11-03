f9df920d879ec16db4998716ad4db224e515341c
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-11-03 22:50:48 +0330

    fix

--- helpers/getUserFixName.js
+++ helpers/getUserFixName.js
@@ -1,9 +1,15 @@
+import cookieHelper from "../../seyed-modules/helpers/cookieHelper"
+
 function getUserFixName({user, short = false})
 {
     if (!short && user?.firstName && user?.lastName) return `${user.firstName} ${user.lastName}`
     else if (user?.firstName) return user.firstName
     else if (user?.lastName) return user.lastName
-    else return "کاربر"
+    else
+    {
+        const lang = cookieHelper.getItem("language") || "fa"
+        return lang === "fa" ? "کاربر" : "user"
+    }
 }
 
 export default getUserFixName
