a25cb43b02fcd65ca4dddf587c1f1cc2f7c67298
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-11-05 15:25:10 +0330

    added disable to radio item

--- components/RadioItem.js
+++ components/RadioItem.js
@@ -2,10 +2,10 @@ import RadioBtnFillSvg from "../media/svg/RadioBtnFillSvg"
 import RadioBtnSvg from "../media/svg/RadioBtnSvg"
 import Material from "../../seyed-modules/components/Material"
 
-function RadioItem({onClick, className, name, isActive, isRtl, rtlExtraContent})
+function RadioItem({onClick, className, name, isActive, isRtl, rtlExtraContent, disable})
 {
     return (
-        <Material className={`select-item ${className}`} onClick={onClick}>
+        <Material className={`select-item ${className}`} disable={disable} onClick={onClick}>
             <div className={`select-item-title ${isRtl ? "rtl" : ""}`}>{name}</div>
             <RadioBtnFillSvg className={`select-item-radio ${isRtl ? "rtl" : ""} ${isActive ? "show" : ""}`}/>
             <RadioBtnSvg className={`select-item-radio ${isRtl ? "rtl" : ""} ${isActive ? "" : "show"}`}/>
