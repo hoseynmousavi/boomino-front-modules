a90eaa73a25a2b0f10a761ae6376403a70310af2
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-11-28 12:30:13 +0330

    added link svg

---
+++ media/svg/LinkSvg.js
@@ -0,0 +1,10 @@
+function LinkSvg({className})
+{
+    return (
+        <svg className={className} viewBox="0 0 19.618 9.809">
+            <path fill="var(--first-icon-color)" strokeWidth="0" d="M6.9 13a4.9 4.9 0 1 0 0 9.809h2.011a.669.669 0 1 0 0-1.338H6.9a3.567 3.567 0 0 1 0-7.134h2.011a.669.669 0 1 0 0-1.338zm7.8 0a.669.669 0 1 0 0 1.338h2.006a3.567 3.567 0 0 1 0 7.134h-1.999a.669.669 0 1 0 0 1.338h2.006a4.9 4.9 0 0 0 0-9.809zm-8.464 4.236a.669.669 0 1 0 0 1.338h11.147a.669.669 0 1 0 0-1.338z" transform="translate(-2 -13)"/>
+        </svg>
+    )
+}
+
+export default LinkSvg
